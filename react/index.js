9ce4302bb47b27b2347bab30917403964fc94166
foxdonut <fdaoud@fastmail.com>
2019-07-08 03:26:16 -0400

    Add react convenience, publish 1.0

--- react/index.js
+++ react/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { sv } from "../src";
 
+const processAttrs = (attrs = {}) => {
+  Object.keys(attrs).forEach(key => {
+    if (key === "onInput") {
+      const value = attrs[key];
+      delete attrs[key];
+      attrs["onChange"] = value;
+    }
+  })
+  return attrs;
+};
+
 export const h = sv(node => {
   if (typeof node === "string") {
     return node;
   }
-  const attrs = node.attrs || {};
+  const attrs = processAttrs(node.attrs);
   if (attrs.innerHTML) {
     attrs.dangerouslySetInnerHTML = { __html: attrs.innerHTML };
     delete attrs.innerHTML;
